a61dd58e51108a9f65921c67d950d0932736c16b
Kris <kris.weber.1994@gmail.com>
2017-08-24 09:59:27 -0700

    Add bodyparser and post route

--- server.js
+++ server.js
@@ -1,11 +1,16 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 const app = express();
+
+app.use(bodyParser.urlencoded({extended: true}));
 
 app.get('/', (req,res) => {
   res.sendFile(__dirname + '/index.html');
 })
 
-
+app.post('/books', (req,res) => {
+  console.log("BOOKS!!!!");
+})
 
 app.listen(3000, function(){
   console.log("listening on port 3000");
